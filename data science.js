a49cd2668a3192e086392fb265c5d877c9aa812c
Ashok044 <132684191+Ashok044@users.noreply.github.com>
2024-07-16 11:01:58 +0530

    Add files via upload

---
+++ data science.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DataScience = () => {
+  return (
+    <div>
+      <h1>Data Science</h1>
+      <p>Content for Data Science...</p>
+    </div>
+  );
+};
+
+export default DataScience;
---
+++ fullstack.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FullStackDevelopment = () => {
+  return (
+    <div>
+      <h1>Full Stack Development</h1>
+      <p>Content for Full Stack Development...</p>
+    </div>
+  );
+};
+
+export default FullStackDevelopment;
